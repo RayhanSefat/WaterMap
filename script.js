29ddc63d30b4f6e2879f8d4fa018b8d74f4baca8
Md. Rayhan Islam Sefat <112571481+RayhanSefat@users.noreply.github.com>
2023-10-05 20:17:33 +0600

    single file

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const L = require('leaflet');
 const map = L.map('map').setView([10.0, 85.0], 5); // Centered on the Bay of Bengal
 
 // Add a tile layer (you can use a different tile provider if desired)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://www.openstreetmap.org/#map=8/22.086/91.505', {
     maxZoom: 19,
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // Example data for a specific zone (you would replace this with real data)
@@ -48,6 +48,6 @@ const zoneCData = {
 const customMarker = L.marker([15.0, 85.0]).addTo(map); // Coordinates for a sample location
 
 // Add a pop-up with data when the marker is clicked
-/*customMarker.bindPopup(`<b>${zoneAData.name}</b><br>Temperature: ${zoneAData.temperature}<br>BOD: ${zoneAData.bod}<br>COD: ${zoneAData.cod}<br>TDS: ${zoneAData.tds}<br>Salinity: ${zoneAData.salinity}<br>Axis: ${zoneAData.x}, ${zoneAData.y}`).openPopup();
+customMarker.bindPopup(`<b>${zoneAData.name}</b><br>Temperature: ${zoneAData.temperature}<br>BOD: ${zoneAData.bod}<br>COD: ${zoneAData.cod}<br>TDS: ${zoneAData.tds}<br>Salinity: ${zoneAData.salinity}<br>Axis: ${zoneAData.x}, ${zoneAData.y}`).openPopup();
 customMarker.bindPopup(`<b>${zoneBData.name}</b><br>Temperature: ${zoneBData.temperature}<br>BOD: ${zoneBData.bod}<br>COD: ${zoneBData.cod}<br>TDS: ${zoneBData.tds}<br>Salinity: ${zoneBData.salinity}<br>Axis: ${zoneBData.x}, ${zoneBData.y}`).openPopup();
-customMarker.bindPopup(`<b>${zoneCData.name}</b><br>Temperature: ${zoneCData.temperature}<br>BOD: ${zoneCData.bod}<br>COD: ${zoneCData.cod}<br>TDS: ${zoneCData.tds}<br>Salinity: ${zoneCData.salinity}<br>Axis: ${zoneCData.x}, ${zoneCData.y}`).openPopup*/();
+customMarker.bindPopup(`<b>${zoneCData.name}</b><br>Temperature: ${zoneCData.temperature}<br>BOD: ${zoneCData.bod}<br>COD: ${zoneCData.cod}<br>TDS: ${zoneCData.tds}<br>Salinity: ${zoneCData.salinity}<br>Axis: ${zoneCData.x}, ${zoneCData.y}`).openPopup();
